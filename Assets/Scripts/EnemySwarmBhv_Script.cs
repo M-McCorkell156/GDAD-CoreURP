4260a4b92aef6d9801620828a5b512faf052178f
M-McCorkell156 <148781370+M-McCorkell156@users.noreply.github.com>
2025-02-04 15:22:25 +0000

    Protying AI group behav

--- Assets/Scripts/EnemySwarmBhv_Script.cs
+++ Assets/Scripts/EnemySwarmBhv_Script.cs
@@ -1,32 +1,105 @@
 using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemySwarmBhv_Script : MonoBehaviour
 {
-
-   //public List<Transform> LiveEnemies;
     public GameObject playerObj;
     public NavMeshAgent agent;
+    public Collider comfortZone;
 
-    private bool alone;
+    [SerializeField] private float checkTimeSecs; 
+    [SerializeField] private GameObject ESTPrefab;
+    private string ESTPrefabName;
+    [SerializeField] private int groupCount; 
 
+    //private GameObject 
 
+    public static HashSet<GameObject> allLiveEST
+
+    private bool isComfort;
+
+    private void Start()
+    {
+        Debug.Log("Begin");
+
+        StartCoroutine(CheckTime());
+    }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (alone)
         {
-
+            //agent.SetDestination()
         }
         else
         {
             transform.LookAt(playerObj.transform.position);
             agent.SetDestination(playerObj.transform.position);
         }
-
-        transform.LookAt(playerObj.transform.position);
-        agent.SetDestination(playerObj.transform.position);
+        */
     }
+
+    private IEnumerator CheckTime()
+    {
+        Debug.Log("Chek time ienum ");
+        yield return new WaitForSeconds(checkTimeSecs);
+
+        PreferredBehaviour();
+
+        StartCoroutine(CheckTime());
+        Debug.Log("Yield return ");
+    }
+
+    private void PreferredBehaviour()
+    {
+        Debug.Log("Start PrefBhv");
+        if (isComfort) // If comfortable charge plyer
+        {
+            transform.LookAt(playerObj.transform.position);
+            agent.SetDestination(playerObj.transform.position);
+            Debug.Log("comfy");
+        }
+        else // not comf find friends 
+        {
+            Debug.Log("lone");          
+        }
+    }
+
+    private void NearestEST()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("there collison");
+        if (other.gameObject.name == ESTPrefabName)
+        {
+            Debug.Log("i got friend");
+            groupCount++;
+        }
+
+        if (groupCount >= 5)
+        {
+            isComfort = true;
+        }
+        else
+        {
+            isComfort = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == ESTPrefabName)
+        {
+            Debug.Log("bye friend");
+            groupCount--;
+        }
+    }
+
 }
